a7cd07b7c4a1d710bc46ae6728c288deb6afb12c
rainbowmori <rainbowmori1218@gmail.com>
2023-11-25 22:42:04 +0900

    Add: something

--- src/main/java/github/rainbowmori/ofro/Ofro.java
+++ src/main/java/github/rainbowmori/ofro/Ofro.java
@@ -1,11 +1,28 @@
 package github.rainbowmori.ofro;
 
+import github.rainbowmori.ofro.config.ConfigManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Ofro extends JavaPlugin {
 
+  private static ConfigManager configManager;
+
+  public static ConfigManager getConfigManager() {
+    return configManager;
+  }
+
+  public static void reloadConfigManager() {
+    configManager.save();
+    configManager = new ConfigManager();
+  }
+
+  public static Ofro getPlugin() {
+    return getPlugin(Ofro.class);
+  }
+
   @Override
   public void onEnable() {
+    configManager = new ConfigManager();
     saveResource("README.txt", true);
   }
 
---
+++ src/main/java/github/rainbowmori/ofro/config/ConfigManager.java
@@ -0,0 +1,13 @@
+package github.rainbowmori.ofro.config;
+
+public class ConfigManager {
+	public final DestinationConfig destinationConfig;
+
+	public ConfigManager() {
+		destinationConfig = new DestinationConfig();
+	}
+
+	public void save() {
+		// 保存する場合はここに書く
+	}
+}
